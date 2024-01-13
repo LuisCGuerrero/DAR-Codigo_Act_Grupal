4e373c476e9b192c6b4b6432b9f1520494d0d13f
Steph <alu0101351728@ull.edu.es>
2024-01-13 16:44:54 +0000

    Register routed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Home from './components/Home';
 import Login from './components/Login';
+import Register from './components/Register';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/register' element={<Register />} />      
         {/* <Route path="*" element={<NoPage />} /> */}
       </Routes>
     </Router>
