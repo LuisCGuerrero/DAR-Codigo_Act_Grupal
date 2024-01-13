a0f4308c1759cfe6e30f05da024d31de8cc9bc93
Steph <alu0101351728@ull.edu.es>
2024-01-13 18:13:30 +0000

    list feed finished

--- src/components/ListFeed.js
+++ src/components/ListFeed.js
@@ -42,10 +42,119 @@ const listDataForYou = [
   },
 ];
 
+const listDataPersonal = [
+  {
+    id: 1,
+    image: "/img/ListFeed/personal1.png",
+    title: "En bucle",
+    description: "Tus canciones favoritas del momento",
+  },
+  {
+    id: 2,
+    image: "/img/ListFeed/personal2.png",
+    title: "Tu capsula del tiempo",
+    description: "Te hemos hecho una lista de reproducción personalizada con canciones para viajar a momentos pasados.",
+  },
+  {
+    id: 3,
+    image: "/img/ListFeed/personal3.png",
+    title: "Ruta diaria",
+    description: "Un mix de noticias y música, hecho para ti"
+  },
+  {
+    id: 4,
+    image: "/img/ListFeed/personal4.png",
+    title: "Una y otra vez",
+    description: "Tus canciones favoritas de antes"
+  }
+];
+
+const listDataRecentlyPLayed = [
+  {
+    id: 1,
+    image: "/img/ListFeed/likedSongs.png",
+    title: "Canciones que te gustan",
+    description: "4077 canciones",
+  },
+  {
+    id: 2,
+    image: "/img/ListFeed/mammaMiaIcon.jpeg",
+    title: "Mamma Mia! The Movie Soundtrack",
+    description: "Cast of Mamma Mia! The Movie",
+  },
+  {
+    id: 3,
+    image: "/img/ListFeed/OT2023.jpeg",
+    title: "Operación Triunfo 2023",
+    description: "Todas las canciones de la Gala 6 y anteriores de OT 2023",
+  },
+  {
+    id: 4,
+    image: "/img/ListFeed/starboy.jpeg",
+    title: "Starboy",
+    description: "The Weeknd",
+  },
+  {
+    id: 4,
+    image: "/img/ListFeed/mp3Emilia.jpeg",
+    title: ".mp3",
+    description: "Emilia",
+  },
+  {
+    id: 5,
+    image: "/img/ListFeed/reputation.jpeg",
+    title: "reputation",
+    description: "Taylor Swift",  
+  }
+]
+
+const listDataPopUpInternational = [
+  {
+    id: 1,
+    image: "/img/ListFeed/1989.jpeg",
+    title: "1989 (Taylor's Version)",
+    description: "Taylor Swift",
+  },
+  {
+    id: 2,
+    image: "/img/ListFeed/guts.jpeg",
+    title: "Guts",
+    description: "Olivia Rodrigo",
+  },
+  {
+    id: 3,
+    image: "/img/ListFeed/popUp.jpeg",
+    title: "Pop Up",
+    description: "Super mega hits! Foto: Ariana Grande",
+  },
+  {
+    id: 4,
+    image: "/img/ListFeed/golden.jpeg",
+    title: "Golden",
+    description: "Jung Kook",
+  },
+  {
+    id: 5,
+    image: "/img/ListFeed/gloria.jpeg",
+    title: "Gloria",
+    description: "Sam Smith",
+  },
+  {
+    id: 6,
+    image: "/img/ListFeed/thisIsHarryStyles.jpg",
+    title: "This Is Harry Styles",
+    description: "This is Harry Styles. Los temas esenciales, todos en una lista.",
+  }
+]
+
+
 function ListFeed() {
   return (
     <div className="list-feed">
       <ListGroup listData={listDataForYou} title={"Hecho para ti"} />
+      <ListGroup listData={listDataPersonal} title={"100% Personal"} />
+      <ListGroup listData={listDataRecentlyPLayed} title={"Escuchado recientemente"} />
+      <ListGroup listData={listDataPopUpInternational} title={"Pop Up: El mejor pop internacional"} />
     </div>
   );
 }
