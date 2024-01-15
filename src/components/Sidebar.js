021732378b8d1be79cd8dfac0fc3e7524b2abf39
monicaaymerich28 <monica.aymerich058@comunidadunir.net>
2024-01-15 13:04:33 +0100

    library-header

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import './styles/Sidebar.css';
-import Logo from './Logo.js'
+import Logo from './Logo.js';
+import LibraryHeader from './LibraryHeader.js';
+
 
 function Sidebar() {
   return (
-      <div className="sidebar">
-        <Logo/>
-        <div className="menu-item">
-          <img src="https://cdn3.emoji.gg/emojis/7184-spotify-home.png" alt="Inicio" className="icon" />
-          <span>Inicio</span>
+    <div className='sidebar'>
+        <div className="sidebarArriba">
+                <div className="logo-inicio">
+                <Logo color="white"/>
+                </div>
+            <div className="menu-item">
+                <img src="https://cdn3.emoji.gg/emojis/7184-spotify-home.png" alt="Inicio" className="icon" />
+                <span>Inicio</span>
+            </div>
+            <div className="menu-item">
+                <img src="https://cdn3.emoji.gg/emojis/3421-spotify-search.png" alt="Buscar" className="icon" />
+                <span>Buscar</span>
+            </div>
         </div>
-        <div className="menu-item">
-          <img src="https://cdn3.emoji.gg/emojis/3421-spotify-search.png" alt="Buscar" className="icon" />
-          <span>Buscar</span>
+
+        <div className='sidebarAbajo'>
+            <LibraryHeader />
+            
         </div>
-      </div>
+    </div>
   );
 }
 
