53439fca5b0e22eaa117928396ec150c22ac3cd0
monicaaymerich28 <monica.aymerich058@comunidadunir.net>
2024-01-15 19:11:56 +0100

    Cambios sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,7 +2,44 @@ import React from 'react';
 import './styles/Sidebar.css';
 import Logo from './Logo.js';
 import LibraryHeader from './LibraryHeader.js';
+import { Link } from 'react-router-dom';
+import like from "./img/like.png"
+import PlaylistItem from './PlaylistItem.js';
 
+
+const ButtonSideBar = () => {
+    return (
+        <div className="btn-sidebar-div">
+          <Link to="/" className="playlist-categoty">Listas</Link>
+          <Link to="/" className="playlist-categoty">Artistas</Link>
+          <Link to="/" className="playlist-categoty">Álbumes</Link>
+          <Link to="/" className="playlist-categoty">Playlist</Link>
+        </div>
+      );
+  };
+
+const playlistData = [
+    { id: 1,
+        image: like,
+        title: "Canciones que te gustan",
+        description: "Playlists",
+    },
+    { id: 2,
+        image: like,
+        title: "Canciones que te gustan",
+        description: "",
+    },
+    { id: 3,
+        image: like,
+        title: "Canciones que te gustan",
+        description: "",
+    },
+    { id: 4,
+        image: like,
+        title: "Canciones que te gustan",
+        description: "",
+    },
+]
 
 function Sidebar() {
   return (
@@ -16,14 +53,27 @@ function Sidebar() {
                 <span>Inicio</span>
             </div>
             <div className="menu-item">
-                <img src="https://cdn3.emoji.gg/emojis/3421-spotify-search.png" alt="Buscar" className="icon" />
+                <img src="httpss://cdn3.emoji.gg/emojis/3421-spotify-search.png" alt="Buscar" className="icon" />
                 <span>Buscar</span>
             </div>
         </div>
 
         <div className='sidebarAbajo'>
             <LibraryHeader />
-            
+            <div className='div-btn'>
+                    <ButtonSideBar />
+            </div>
+            <div className='playlist-feed'>
+                {playlistData.map((playlistData) => (
+                    <PlaylistItem
+                       key={playlistData.id}
+                        image={playlistData.image}
+                        title={playlistData.title}
+                        description={playlistData.description}
+                    />
+                ))}
+                
+            </div>
         </div>
     </div>
   );
